868f3ff88a860d972c924d414daf1f8002fef023
Alexander Nihlén <alex.nihlen@gmail.com>
2021-12-26 01:59:00 +0100

    Added message project

---
+++ Nihlen.Message/BF2WebAdminMessages.cs
@@ -0,0 +1,27 @@
+﻿// ReSharper disable All
+namespace Nihlen.Message;
+
+// TODO: keep messages in sync with Nihlen.Common / github nuget?
+
+/// <summary>
+/// Start a stream a game or optional match
+/// </summary>
+public record StartGameStream
+{
+    public string GameServerIp { get; init; }
+    public int GameServerPort { get; init; }
+    public string GameServerPassword { get; init; }
+    public int GameQueryPort { get; init; }
+    public Guid? MatchId { get; init; }
+    public string? MatchMode { get; init; }
+}
+
+/// <summary>
+/// Stop stream for the specified server
+/// </summary>
+public record StopGameStream
+{
+    public string GameServerIp { get; init; }
+    public int GameServerPort { get; init; }
+    public Guid? MatchId { get; init; }
+}
